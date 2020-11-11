947c48e3c3a874ae8e13e683815b6b556bd96851
girok21 <34954382+girok21@users.noreply.github.com>
2020-11-11 13:50:23 +0530

    max subarray sum using kadane's algorithm
    https://practice.geeksforgeeks.org/problems/kadanes-algorithm-1587115620/1#

--- array/maxsubarray.cpp
+++ array/maxsubarray.cpp
@@ -1,1 +1,39 @@
+#include<bits/stdc++.h>
+using namespace std;
 
+int maxSubarraySum(int arr[], int n){
+    
+    // Your code here
+    int sum = 0;//current subarray sum
+    int max = arr[0];//maximum subarray sum
+    for(int i=0;i<n;i++)
+    {
+        sum+=arr[i];
+        if(sum>max)
+        {
+            max=sum;
+        }
+        if(sum<0)//wont make any difference if i add anything to this sum
+            sum=0;
+    }
+    return max;
+}
+
+int main()
+{
+    int t,n;
+    
+    cin>>t; //input testcases
+    while(t--) //while testcases exist
+    {
+        
+        cin>>n; //input size of array
+        
+        int a[n];
+        
+        for(int i=0;i<n;i++)
+            cin>>a[i]; //inputting elements of array
+        
+        cout << maxSubarraySum(a, n) << endl;
+    }
+}
